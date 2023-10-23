ddb2e3e83677afd99a3b92d488d5b8f016afaaa4
Korbo <korbo@ooer.ooo>
2023-10-23 15:48:00 -0500

    skin functionality added, have yet to make it accessable

--- source/main.c
+++ source/main.c
@@ -8,13 +8,6 @@
 
 #include "main.h"
 
-
-#define DARK_BG 0x282828FF
-#define LIGHT_BG 0xF5EDCAFF
-#define DARK_FG 0xD4BE98FF
-#define LIGHT_FG 0x644735FF
-#define GRRLIB_WHITE 0xFFFFFFFF
-
 int frameIndex = 0;
 
 int main(int argc, char **argv) {
@@ -34,32 +27,33 @@ int main(int argc, char **argv) {
     unsigned int shigCount = 0;
     char str[38];
     int theme = 1;
-    double sx = 0, sy = 0, sa = 0;
     signed int timerSeconds = -3;
     WPADData *wd;
     GRRLIB_ttfFont* font = GRRLIB_LoadTTF(font_ttf, font_ttf_size);
     AESNDPB* shigCompleteVoice;
-
+    struct shiggyData shiggy;
+    shiggy.x = shiggy.y = shiggy.angle = shiggy.skin = 0;
+    int texture; // aaaaAAAAAHHH
     while (1) {
         timer++;
         if (timer >= 60) {
             timer = 0;
             --timerSeconds;
             if ((timerSeconds < -1) & !(timerSeconds < -2)) timerSeconds = -1;
-            if (timerSeconds < -3) timerSeconds = -3;
+            if ((timerSeconds < -3) & !(timerSeconds <= -4)) timerSeconds = -3;
         }
         WPAD_ScanPads();
         PAD_ScanPads();
         wd = WPAD_Data(0);
         if (wd->ir.valid) {
-            sx = wd->ir.x;
-            sy = wd->ir.y;
-            sa = wd->ir.angle;
+            shiggy.x = wd->ir.x;
+            shiggy.y = wd->ir.y;
+            shiggy.angle = wd->ir.angle;
         }
-        if (abs(PAD_StickX(0)) > 25) sx += PAD_StickX(0)/20;
-        if (abs(PAD_StickY(0)) > 25) sy -= PAD_StickY(0)/20;
-        if (PAD_ButtonsHeld(0) & PAD_TRIGGER_L) --sa;
-        if (PAD_ButtonsHeld(0) & PAD_TRIGGER_R) ++sa;
+        if (abs(PAD_StickX(0)) > 25) shiggy.x += PAD_StickX(0)/20;
+        if (abs(PAD_StickY(0)) > 25) shiggy.y -= PAD_StickY(0)/20;
+        if (PAD_ButtonsHeld(0) & PAD_TRIGGER_L) --shiggy.angle;
+        if (PAD_ButtonsHeld(0) & PAD_TRIGGER_R) ++shiggy.angle;
 
         if (theme) {
             GRRLIB_FillScreen(DARK_BG);
@@ -71,7 +65,7 @@ int main(int argc, char **argv) {
             break;
         } else if ((WPAD_ButtonsDown(0) != 0) || (PAD_ButtonsDown(0) != 0)) {
             if ((WPAD_ButtonsDown(0) & WPAD_BUTTON_A) || (PAD_ButtonsDown(0) & PAD_BUTTON_A)) {
-                if (GRRLIB_PtInRect(536, 384, 64, 64, sx, sy)) {
+                if (GRRLIB_PtInRect(536, 384, 64, 64, shiggy.x, shiggy.y)) {
                     ++theme;
                     theme %= 2;
                 }
@@ -81,14 +75,29 @@ int main(int argc, char **argv) {
             timerSeconds = 59;
             timer = 0;
         }
-            if ((WPAD_ButtonsDown(0) & WPAD_BUTTON_2) || (PAD_ButtonsDown(0) & PAD_TRIGGER_Z)) sx = sy = sa = 0;
+            if ((WPAD_ButtonsDown(0) & WPAD_BUTTON_2) || (PAD_ButtonsDown(0) & PAD_TRIGGER_Z)) shiggy.x = shiggy.y = shiggy.angle = 0;
             ++frameIndex;
             frameIndex %= frameCount;
             if (!frameIndex & (timerSeconds > -1)) {
                 shigCompleteVoice = AESND_AllocateVoice(VoiceCallBack);
                 if (shigCompleteVoice) {
-
-                    AESND_PlayVoice(shigCompleteVoice, VOICE_STEREO16, &concrete_raw, concrete_raw_size, 48000, 0, 0);
+                    switch (shiggy.skin) {
+                        case 0:
+                            AESND_PlayVoice(shigCompleteVoice, VOICE_STEREO16, concrete_raw, concrete_raw_size, 48000, 0, 0);
+                            break;
+                        case 1:
+                            AESND_PlayVoice(shigCompleteVoice, VOICE_STEREO16, concrete_glitch_raw, concrete_glitch_raw_size, 48000, 0, 0);
+                            break;
+                        case 2:
+                            AESND_PlayVoice(shigCompleteVoice, VOICE_STEREO16, explod_raw, explod_raw_size, 48000, 0, 0);
+                            break;
+                        case 3:
+                            AESND_PlayVoice(shigCompleteVoice, VOICE_STEREO16, concrete_lowquality_raw, concrete_lowquality_raw_size, 48000, 0, 0);
+                            break;
+                        case 4:
+                            AESND_PlayVoice(shigCompleteVoice, VOICE_STEREO16, concrete_reversed_raw, concrete_reversed_raw_size, 48000, 0, 0);
+                            break;
+                    }
                 }
                 ++shigCount;
             }
@@ -98,17 +107,33 @@ int main(int argc, char **argv) {
         }
 
         GRRLIB_texImg *themeButtonTex = themeButtonIndex[theme];
-
         GRRLIB_SetAntiAliasing(false);
-        GRRLIB_DrawPart(sx+320, sy, 0+(64*frameIndex), 0, 64, 64, shiggyTex, sa, 1, 1, GRRLIB_WHITE);
-        GRRLIB_SetMidHandle(shiggyTex, true);
+        switch (shiggy.skin) {
+            case 0:
+                texture = 0;
+                break;
+            case 1:
+                texture = 1;
+                break;
+            case 2:
+                texture = 2;
+                break;
+            case 3:
+                texture = 0;
+                break;
+            case 4:
+                texture = 3;
+                break;
+}
+        GRRLIB_DrawPart(shiggy.x+320, shiggy.y, 0+(64*frameIndex), 0, 64, 64, shiggyTex[texture], shiggy.angle, 2, 2, GRRLIB_WHITE);
+        GRRLIB_SetMidHandle(shiggyTex[texture], true);
         if (timerSeconds > -1) {
-            double deciSeconds = 10-((double)timer/60*10);
-            sprintf(str, "%u shigs, %u.%us left", shigCount, timerSeconds, (unsigned int)deciSeconds);
+            double deciseconds = 10-((double)timer/60*10);
+            sprintf(str, "%u shigs, %u.%us left", shigCount, timerSeconds, (int)deciseconds);
         } else if (timerSeconds == -1) {
             sprintf(str, "%u shigs in 60s", shigCount);
-        } else sprintf(str, "Press +/START to start timer");
-        GRRLIB_SetAntiAliasing(true);
+        } else sprintf(str, "+/START: Start Timer, -/B: Infinite");
+        GRRLIB_SetAntiAliasing(shiggy.skin != 4);
         if (theme) {
             GRRLIB_DrawImg(536, 384, themeButtonTex, 0, 1, 1, DARK_FG);
             GRRLIB_PrintfTTF(320-(GRRLIB_WidthTTF(font, str, 24)/2), 32, font, str, 24, DARK_FG);
@@ -117,7 +142,7 @@ int main(int argc, char **argv) {
             GRRLIB_PrintfTTF(320-(GRRLIB_WidthTTF(font, str, 24)/2), 32, font, str, 24, LIGHT_FG);
         }
         GRRLIB_Render();
-        // for (int i = 1; i < 20; i++) VIDEO_WaitVSync();
+        // for (int i = 1; i < 20; i++) VIDEO_WaitVsync();
     }
     WPAD_Shutdown();
     free_textures();
--- source/main.h
+++ source/main.h
@@ -1,24 +1,51 @@
 #include <grrlib.h>
 
-#include "shiggy_png.h"
+#include "shiggy0_png.h"
+#include "shiggy1_png.h"
+#include "shiggy2_png.h"
+#include "shiggy3_png.h"
 #include "bz_pif_it.h"
 #include "font_ttf.h"
 #include "light_png.h"
 #include "dark_png.h"
 #include "concrete_raw.h"
+#include "concrete_lowquality_raw.h"
+#include "concrete_reversed_raw.h"
+#include "concrete_glitch_raw.h"
+#include "explod_raw.h"
+
+#define DARK_BG 0x282828FF
+#define LIGHT_BG 0xF5EDCAFF
+#define DARK_FG 0xD4BE98FF
+#define LIGHT_FG 0x644735FF
+#define GRRLIB_WHITE 0xFFFFFFFF
+
+double aspectRatioScale(double x) {return x/(4.f/3.f);}
 
 GRRLIB_texImg *themeButtonIndex[2];
-GRRLIB_texImg *shiggyTex;
+GRRLIB_texImg *shiggyTex[4];
+
+struct shiggyData {
+    double x, y, angle;
+    int skin;
+};
 
 void init_textures() {
     themeButtonIndex[0] = GRRLIB_LoadTexture(light_png);
     themeButtonIndex[1] = GRRLIB_LoadTexture(dark_png);
-    shiggyTex = GRRLIB_LoadTexture(shiggy_png);
+
+    shiggyTex[0] = GRRLIB_LoadTexture(shiggy0_png);
+    shiggyTex[1] = GRRLIB_LoadTexture(shiggy1_png);
+    shiggyTex[2] = GRRLIB_LoadTexture(shiggy2_png);
+    shiggyTex[3] = GRRLIB_LoadTexture(shiggy3_png);
 }
 
 void free_textures() {
-    for (int i = 0; i < 1; i++) {
+    for (int i = 0; i < 2; i++) {
         GRRLIB_FreeTexture(themeButtonIndex[i]);
+    }
+    for (int i = 0; i < 3; i++) {
+        GRRLIB_FreeTexture(shiggyTex[i]);
     }
 }
 
