6710994d6bfd8e2258e66c2675c16ba17df4a6d3
Korbin Marshall <korbin.m.marshall@gmail.com>
2023-12-27 12:05:24 -0600

    aa

--- source/main.c
+++ source/main.c
@@ -9,32 +9,28 @@
 #include "main.h"
 
 int HWButton = -1;
-struct shiggyData shiggy;
+struct ShiggyData shiggy;
 int frameCount = 10;
 int frameIndex = 0;
 signed int timerSeconds = -3;
 int theme = 1;
 AESNDPB* shigCompleteVoice;
 int timer = 0;
+unsigned int skinNum = 0;
 
 /**
  * handler for controls so my code is less shit lmao
  */
 void buttonHandler() {
     if ((WPAD_ButtonsHeld(0) & WPAD_BUTTON_A) || (PAD_ButtonsHeld(0) & PAD_BUTTON_A)) {
-        if (shiggy.hyperTimer == 0) shiggy.hyperTimer = 10;
+        if (shiggy.hyperTimer == 0) shiggy.hyperTimer = 9;
     }
     if ((WPAD_ButtonsDown(0)) || (PAD_ButtonsDown(0))) {
-        if ((WPAD_ButtonsDown(0) & WPAD_BUTTON_A) || (PAD_ButtonsDown(0) & PAD_BUTTON_A)) {
-            if (GRRLIB_PtInRect(536, 384, 64, 64, shiggy.x, shiggy.y)) {
-                ++theme;
-                theme %= 2;
-            }
-        }
     if ((WPAD_ButtonsDown(0) & WPAD_BUTTON_PLUS) || (PAD_ButtonsDown(0) & PAD_BUTTON_START)) {
         if ((WPAD_ButtonsHeld(0) & WPAD_BUTTON_B) || (PAD_ButtonsHeld(0) & PAD_BUTTON_Y)) {
-            ++shiggy.skinNum;
-            shiggy.skinNum %= SHIGGY_SKINS;
+            ++skinNum;
+            skinNum %= SKIN_AMOUNT;
+            shiggy.Skin = shiggySkins[skinNum];
             return;
         }
         shiggy.shigCount = 0;
@@ -43,8 +39,9 @@ void buttonHandler() {
     }
     if ((WPAD_ButtonsDown(0) & WPAD_BUTTON_MINUS) || (PAD_ButtonsDown(0) & PAD_BUTTON_B)) {
         if ((WPAD_ButtonsHeld(0) & WPAD_BUTTON_B) || (PAD_ButtonsHeld(0) & PAD_BUTTON_Y)) {
-            --shiggy.skinNum;
-            if ((signed)shiggy.skinNum < 0) shiggy.skinNum = SHIGGY_SKINS-1;
+            --skinNum;
+            if ((signed)skinNum < 0) skinNum = SKIN_AMOUNT-1;
+            shiggy.Skin = shiggySkins[skinNum];
             return;
         }
         shiggy.shigCount = 0;
@@ -56,20 +53,20 @@ void buttonHandler() {
     frameIndex %= frameCount;
     if (!frameIndex & ((timerSeconds >= 0) || (timerSeconds <= -5))) {
         shigCompleteVoice = AESND_AllocateVoice(VoiceCallBack);
-        if (shigCompleteVoice) switch (shiggy.skinNum) {
-                case 1:
+        if (shigCompleteVoice) switch (shiggy.Skin.sndNum) {
+                case SOUND_GLITCH:
                     AESND_PlayVoice(shigCompleteVoice, VOICE_STEREO16, concrete_glitch_raw, concrete_glitch_raw_size, 48000, 0, 0);
                     break;
-                case 2:
+                case SOUND_EXPLOD:
                     AESND_PlayVoice(shigCompleteVoice, VOICE_STEREO16, explod_raw, explod_raw_size, 48000, 0, 0);
                     break;
-                case 3:
+                case SOUND_LOWQUALITY:
                     AESND_PlayVoice(shigCompleteVoice, VOICE_STEREO16, concrete_lowquality_raw, concrete_lowquality_raw_size, 48000, 0, 0);
                     break;
-                case 4:
-                    AESND_PlayVoice(shigCompleteVoice, VOICE_STEREO16, concrete_reversed_raw, concrete_reversed_raw_size, 48000, 0, 0);
+                case SOUND_REVERSE:
+                    AESND_PlayVoice(shigCompleteVoice, VOICE_STEREO16, concrete_reverse_raw, concrete_reverse_raw_size, 48000, 0, 0);
                     break;
-                case 5:
+                case SOUND_TROLOLO:
                     AESND_PlayVoice(shigCompleteVoice, VOICE_STEREO16, yayayayay_raw, yayayayay_raw_size, 48000, 0, 0);
                     break;
                 default:
@@ -111,8 +108,9 @@ void WiimotePowerPressed(s32 chan) {
  */
 int main(int argc, char **argv) {
 // init video
-    GRRLIB_Init();
     init_textures();
+    modify_textures();
+    GRRLIB_Init();
 // init input
     WPAD_Init();
     PAD_Init();
@@ -128,10 +126,11 @@ int main(int argc, char **argv) {
     GRRMOD_Start();
 // the variable chunk (oooh! ahhh!)
     char str[38];
-    int texture = 0; // aaaaAAAAAHHH
+    // int texture = 0; // aaaaAAAAAHHH
     WPADData *wd;
     GRRLIB_ttfFont* font = GRRLIB_LoadTTF(font_ttf, font_ttf_size);
-    shiggy.x = shiggy.y = shiggy.angle = shiggy.skinNum = shiggy.shigCount = shiggy.hyperTimer = 0; // lmfao
+    shiggy.x = shiggy.y = shiggy.angle = shiggy.shigCount = shiggy.hyperTimer = 0; // lmfao
+    shiggy.Skin = shiggySkins[0];
 // main loop
     while (1) {
         timer++;
@@ -157,36 +156,12 @@ int main(int argc, char **argv) {
         if (PAD_ButtonsHeld(0) & PAD_TRIGGER_R) ++shiggy.angle;
         if (shiggy.hyperTimer) shiggy.hyperTimer--;
 
-        if (theme) {
-            GRRLIB_FillScreen(DARK_BG);
-        } else {
-            GRRLIB_FillScreen(LIGHT_BG);
-        }
-
         if (WPAD_ButtonsDown(0) & WPAD_BUTTON_HOME) {
             // break;
         } else if (WPAD_ButtonsHeld(0) || PAD_ButtonsHeld(0)) buttonHandler();
-
-        GRRLIB_texImg *themeButtonTex = themeButtonIndex[theme];
-        GRRLIB_SetAntiAliasing(shiggy.skinNum == 3);
-        switch (shiggy.skinNum) {
-            case 1:
-                texture = 1;
-                break;
-            case 2:
-                texture = 2;
-                break;
-            case 4:
-                texture = 3;
-                break;
-            case 5:
-                break;
-            default:
-                texture = 0;
-                break;
-        }
-        GRRLIB_DrawPart(shiggy.x+320, shiggy.y-(hyperYCoords[10 - shiggy.hyperTimer]), frameIndex*64, 0, 64, 64, shiggyTex[texture], shiggy.angle, 2, 2, GRRLIB_WHITE);
-        GRRLIB_SetMidHandle(shiggyTex[texture], true);
+        GRRLIB_SetAntiAliasing(shiggy.Skin.blurred);
+        GRRLIB_DrawPart(shiggy.x+320, shiggy.y-(hyperYCoords[10 - shiggy.hyperTimer]), frameIndex*64, 0, 64, 64, shiggyTex[shiggy.Skin.texNum], shiggy.angle, 2, 2, GRRLIB_WHITE);
+        GRRLIB_SetMidHandle(shiggyTex[shiggy.Skin.texNum], true);
         if (timerSeconds > TIMER_STATE_TIMEOVER) {
             double deciseconds = 10-((double)timer/60*10);
             if (deciseconds >= 10) deciseconds = 9;
@@ -196,14 +171,8 @@ int main(int argc, char **argv) {
         } else if (timerSeconds <= TIMER_STATE_GAMEOVER) {
             sprintf(str, "%u shigs", shiggy.shigCount);
         } else sprintf(str, "+/START: Start Timer, -/B: Infinite");
-        GRRLIB_SetAntiAliasing(shiggy.skinNum != 4);
-        if (theme) {
-            GRRLIB_DrawImg(536, 384, themeButtonTex, 0, 1, 1, DARK_FG);
-            GRRLIB_PrintfTTF(320-(GRRLIB_WidthTTF(font, str, 24)/2), 32, font, str, 24, DARK_FG);
-        } else {
-            GRRLIB_DrawImg(536, 384, themeButtonTex, 0, 1, 1, LIGHT_FG);
-            GRRLIB_PrintfTTF(320-(GRRLIB_WidthTTF(font, str, 24)/2), 32, font, str, 24, LIGHT_FG);
-        }
+        GRRLIB_SetAntiAliasing(false);
+        GRRLIB_PrintfTTF(320-(GRRLIB_WidthTTF(font, str, 24)/2), 32, font, str, 24, DARK_FG);
         GRRLIB_Render();
         // for (int i = 1; i < 20; i++) VIDEO_WaitVsync();
     }
--- source/main.h
+++ source/main.h
@@ -3,14 +3,11 @@
 #include "shiggy1_png.h"
 #include "shiggy2_png.h"
 #include "shiggy3_png.h"
-#include "shiggy4_png.h"
 #include "bz_pif_it.h"
 #include "font_ttf.h"
-#include "light_png.h"
-#include "dark_png.h"
 #include "concrete_raw.h"
 #include "concrete_lowquality_raw.h"
-#include "concrete_reversed_raw.h"
+#include "concrete_reverse_raw.h"
 #include "concrete_glitch_raw.h"
 #include "yayayayay_raw.h"
 #include "explod_raw.h"
@@ -18,15 +15,19 @@
 #define TIMER_STATE_GAMEOVER -4
 #define TIMER_STATE_TIMEOVER -1
 
-#define SHIGGY_SKINS 6
-#define DARK_BG 0x282828FF
-#define LIGHT_BG 0xF5EDCAFF
-#define DARK_FG 0xD4BE98FF
-#define LIGHT_FG 0x644735FF
-#define GRRLIB_WHITE 0xFFFFFFFF
+#define SOUND_DEFAULT    0
+#define SOUND_GLITCH     1
+#define SOUND_EXPLOD     2
+#define SOUND_LOWQUALITY 3
+#define SOUND_REVERSE    4
+#define SOUND_TROLOLO    5
 
-GRRLIB_texImg *themeButtonIndex[2];
-GRRLIB_texImg *shiggyTex[5];
+#define DARK_BG      0x282828FF
+#define DARK_FG      0xD4BE98FF
+#define GRRLIB_WHITE 0xFFFFFFFF
+#define SKIN_AMOUNT 6
+
+GRRLIB_texImg *shiggyTex[6];
 
 int hyperYCoords[10] = {
     0,
@@ -41,27 +42,47 @@ int hyperYCoords[10] = {
     10
 };
 
-struct shiggyData {
+struct ShiggySkin {
+    unsigned int texNum, sndNum;
+    bool blurred;
+    char *name[10];
+};
+
+struct ShiggyData {
     double x, y, angle;
-    unsigned int skinNum, shigCount, hyperTimer;
+    int shigCount;
+    unsigned int hyperTimer;
+    struct ShiggySkin Skin;
+};
+
+struct ShiggySkin shiggySkins[6] = {
+    {0, SOUND_DEFAULT,    false, false,     {"Shiggy"}},
+    {1, SOUND_GLITCH,     false, false,  {"Corrupted"}},
+    {2, SOUND_EXPLOD,     false, false, {"Shigsplode"}},
+    {5, SOUND_LOWQUALITY, true,  false,     {"Shoggy"}},
+    {3, SOUND_REVERSE,    false, false,     {"yggihS"}},
+    {4, SOUND_TROLOLO,    false, false,    {"Shiggey"}}
 };
 
 void init_textures() {
-    themeButtonIndex[0] = GRRLIB_LoadTexture(light_png);
-    themeButtonIndex[1] = GRRLIB_LoadTexture(dark_png);
-
     shiggyTex[0] = GRRLIB_LoadTexture(shiggy0_png);
     shiggyTex[1] = GRRLIB_LoadTexture(shiggy1_png);
     shiggyTex[2] = GRRLIB_LoadTexture(shiggy2_png);
-    shiggyTex[3] = GRRLIB_LoadTexture(shiggy3_png);
-    shiggyTex[4] = GRRLIB_LoadTexture(shiggy4_png);
+    shiggyTex[3] = GRRLIB_CreateEmptyTexture(shiggyTex[0]->w, shiggyTex[0]->h);
+    shiggyTex[4] = GRRLIB_LoadTexture(shiggy3_png);
+    shiggyTex[5] = GRRLIB_CreateEmptyTexture(shiggyTex[0]->w, shiggyTex[0]->h);
+    GRRLIB_BMFX_FlipH(shiggyTex[0], shiggyTex[3]);
+    GRRLIB_FlushTex(shiggyTex[3]);
+    GRRLIB_BMFX_Blur(shiggyTex[0], shiggyTex[5], 1);
+    GRRLIB_FlushTex(shiggyTex[5]);
+}
+
+void modify_textures() {
+
 }
 
 void free_textures() {
-    for (int i = 0; i < 2; i++) {
-        GRRLIB_FreeTexture(themeButtonIndex[i]);
-    }
-    for (int i = 0; i < 3; i++) {
+    for (int i = 0; i <= 4; i++) {
         GRRLIB_FreeTexture(shiggyTex[i]);
     }
 }
