38b63348c4e0adc3c710f2a3c52c5df17c5deabc
Korbo <korbo@ooer.ooo>
2024-07-16 07:43:32 -0500

    guhhh

---
+++ source/resource.h
@@ -0,0 +1,46 @@
+
+// Spritesheets & Fonts
+
+#include "shiggy0_png.h"
+#include "shiggy1_png.h"
+#include "shiggy2_png.h"
+#include "shiggy3_png.h"
+#include "shiggy4_png.h"
+#include "aro_png.h"
+#include "ace_png.h"
+#include "aroace_png.h"
+#include "agndr_png.h"
+#include "trans_png.h"
+#include "lgbtq_png.h"
+#include "gay_png.h"
+#include "les_png.h"
+#include "one_bpp_png.h"
+#include "two_bpp_png.h"
+#include "vennie_png.h"
+#include "bi_png.h"
+
+#include "font_png.h"
+
+// Sounds & Music
+
+#include "bz_pif_it.h"
+
+#include "concrete_raw.h"
+#include "concrete_lowquality_raw.h"
+#include "concrete_reverse_raw.h"
+#include "concrete_glitch_raw.h"
+#include "yayayayay_raw.h"
+#include "explod_raw.h"
+
+#define SOUND_DEFAULT    0
+#define SOUND_GLITCH     1
+#define SOUND_EXPLOD     2
+#define SOUND_LOWQUALITY 3
+#define SOUND_REVERSE    4
+#define SOUND_TROLOLO    5
+
+// Colors
+
+#define DARK_BG      0x282828FF
+#define DARK_FG      0xD4BE98FF
+#define WHITE        0xFFFFFFFF
